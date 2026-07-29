6f33b1282686dd1d26c662ddf937ae1572ec5298
marco-hp <marco.dinh.hp@gmail.com>
2026-07-29 21:04:34 +1000

    bare-prom-client

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-if (global.Bare) {
-  require('bare-process/global')
-}
 const test = require('brittle')
 const crypto = require('hypercore-crypto')
 const HyperDHT = require('hyperdht')
 const setupTestnet = require('hyperdht/testnet')
-const promClient = require('prom-client', {
-  with: { imports: './package.json' }
-})
+const promClient = require('prom-client')
 
 const { HyperDiscovery, HyperDiscoveryClient } = require('.')
 
